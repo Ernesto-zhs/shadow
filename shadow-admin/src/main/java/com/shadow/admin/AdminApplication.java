0da283c0e0c6b8bb1091e8809ba9444094324e63
Ernesto-zhs <1203828080@qq.com>
2019-07-24 23:46:35 +0800

    1.移除多余的pom配置文件 2.连接池修改为spring boot 2.0默认链接池 3.启动改为继承springBootServletInitializer

--- shadow-admin/src/main/java/com/shadow/admin/AdminApplication.java
+++ shadow-admin/src/main/java/com/shadow/admin/AdminApplication.java
@@ -3,13 +3,19 @@ package com.shadow.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan(basePackages = "com.shadow.dao")
 @SpringBootApplication(scanBasePackages = "com.shadow.admin.controller")
-public class AdminApplication {
+public class AdminApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
     }
-
 }
